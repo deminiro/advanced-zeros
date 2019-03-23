0064b56aa8e57d0c3fefa12bf4311d4f098006c2
deminiro <43145879+deminiro@users.noreply.github.com>
2019-03-23 22:06:24 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  if(number>=1 && number<=10^7 && base>=2 && base<=256){
+    
+    var divides = [];
+    var j = 1;
+    var i = 2;
+    for(j;i<base&&j<20;j++){
+    if(base % 2 === 1){
+      i++;
+      if(i<20){
+        for(var k=3;base/k % 2 === 0;k+=2){
+        divides.push(base/k);
+        break;
+        }
+      }
+      else if(i>=20){
+        divides.push(base);
+      }
+    
+    }else{
+      base = Math.floor(base/i);
+      divides.push(i);
+    }
+    }
+    let countOfTwo = 0;
+    for(two=2;two<=number;two*=2){
+    
+      countOfTwo+= Math.floor(number/two);
+    } 
+
+    const srsa = Math.floor(countOfTwo/1);//Полученное число(29строка) из цикла делим на кол-во двоек с base
+    let countOfAnother = 0;
+    for(another=divides[1];another<=number;another*=divides[1]){
+      countOfAnother+= Math.floor(number/another);
+    }
+    srsb = countOfAnother;
+
+    const numbers = [srsa, srsb];
+
+    numbers.sort(function(a, b) {
+      return b - a;
+    });
+
+    return numbers[0];
+}
 }
